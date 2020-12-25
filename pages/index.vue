2e1f65c21fd3a20613d1a4b7b7d55ce342ba0cc3
Jacob Rogaishio <me@jacobrogaishio.com>
2020-12-25 14:41:40 -0500

    Continue design

--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,114 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+    <v-col cols="12">
       <div class="text-center">
         <img
         class="logo"
         alt="Bevy Chat Logo"
         src="/logo_color.png" />
       </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+    </v-col>
+
+    <v-col cols="12">
+        <v-row>
+            <v-col cols="6" class="text-right">
+                <h1 class='text-h1 bevy-title mb-2'>Bevy</h1>
+                <h2><span class="text-muted pr-2 phonetic font-weight-light">/ˈbevē/</span></h2>
+                <blockquote class='mr-4'>
+                    <p>
+                        <h3 class="bevy-type"><em>noun</em></h3>
+                    </p>
+                    <p>
+                        <ol class='pr-3 definition'>
+                            <li>
+                                <span class="text-muted">1.</span> large group or collection
+                            </li>
+                            <li>
+                                <span class="text-muted">2.</span> a group of animals and especially quail
+                            </li>
+                        </ol>
+                    </p>
+                </blockquote>
+            </v-col>
+
+            <v-col cols="6">
+                <h1 class='text-h1 bevy-title mb-2'>Chat</h1>
+                <h2><span class="text-muted pl-2 phonetic font-weight-light">/CHat/</span></h2>
+                <blockquote class='ml-4'>
+                    <p>
+                        <h3 class="bevy-type"><em>verb</em></h3>
+                    </p>
+                    <p>
+                        <ol class='pl-3 definition'>
+                            <li>
+                                <span class="text-muted">1.</span> talk in a friendly and informal way.
+                            </li>
+                            <li>
+                                <span class="text-muted">2.</span> to take part in an online discussion in a chat room
+                            </li>
+                        </ol>
+                    </p>
+                </blockquote>
+            </v-col>
+        </v-row>
     </v-col>
   </v-row>
 </template>
+
+<style>
+    >>>.text-muted {
+        color:#333 !important;
+    }
+
+    .row.body {
+        padding-bottom:20px;
+    }
+    .bevy-title {
+        font-size: 5em;
+        color:#000;
+            font-family: "Nunito", sans-serif;
+    }
+    .bevy-type {
+        color:#333;
+    }
+    .phonetic {
+        color:#fff !important;
+    }
+    ol.definition {
+        color: #000;
+    }
+    ol.definition > li > span.text-muted {
+        color:#333 !important;
+    }
+
+    @media screen and (max-width: 425px) {
+        .links > a {
+            padding:0 15px;
+        }
+    }
+
+    @media screen and (min-width: 426px) {
+        .links > a {
+            padding: 0 25px;
+        }
+    }
+
+    .links > a {
+        color: #fff;
+        font-size: 13px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+    .links {
+        margin-top:20px;
+    }
+    ol {
+        list-style-type: none;
+    }
+
+</style>
 
 <script>
 //import Logo from '~/components/Logo.vue'
